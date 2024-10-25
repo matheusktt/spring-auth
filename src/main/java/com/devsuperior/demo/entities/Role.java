631a05ac550b2f2d3e442f94fc004198b258d771
matheusktt <matheus_0119@outlook.com>
2024-10-25 19:22:52 -0300

    implementing GrantedAuthority and UserDetails

--- src/main/java/com/devsuperior/demo/entities/Role.java
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,12 +1,14 @@
 package com.devsuperior.demo.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_user")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,6 +32,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
